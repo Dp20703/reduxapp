61dde39021b93776fb84cfa41504d11a6cef116e
Darshan <darshanrp2003@gmail.com>
2024-06-06 16:09:38 +0530

    Started Reduxapp and created Navbar and Shop

---
+++ src/components/shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className='container'>
+            <h2>Adidas Shoes 50Rs:</h2>
+            <button className="btn btn-primary mx-2">-</button>
+             Add to Cart
+            <button className="btn btn-primary mx-2">+</button>
+
+        </div>
+    )
+}
+
+export default Shop
